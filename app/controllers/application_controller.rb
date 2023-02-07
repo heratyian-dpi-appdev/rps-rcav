dc969ee925dfb613b2b25b68c0aaeb59fd4f252b
Ian <heratyian@gmail.com>
2023-02-07 22:27:40 +0000

    Added wrapper layout for root, rock, paper, and scissors

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,22 @@
 class ApplicationController < ActionController::Base
-  layout(false)
+  layout("wrapper")
 
   # Add your actions below this line
   # ================================
 
+  def root
+    render({ template: "game_templates/root" })
+  end
+
+  def play_rock
+    render({ template: "game_templates/rock" })
+  end
+
+  def play_paper
+    render({ template: "game_templates/paper" })
+  end
+
+  def play_scissors
+    render({ template: "game_templates/scissors" })
+  end
 end
